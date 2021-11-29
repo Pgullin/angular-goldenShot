c9e95c4bcd650860a73f1dd59657a5867c9ecce4
Petter Gullin <petter.gullin@gmail.com>
2021-11-29 13:57:04 +0100

    Initial commit

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {GoldenShot} from '../goldenShot';
+import {Observable} from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  private goldenShotUrl = 'http://localhost:4221/goldenShot';
+
+  constructor(private http: HttpClient) { }
+
+  getGoldenShots(): Observable<GoldenShot[]> {
+    return this.http.get<GoldenShot[]>(this.goldenShotUrl);
+  }
+}
